db123f826fb8b79c4a86daf4ea72a261e86144c8
irpsv <i.rpsv@live.com>
2016-12-18 14:45:50 +0500

    add Filter

---
+++ src/Filter.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace jugger\data;
+
+/**
+ * фильтр
+ * объект который содержит в себе список фильтров
+ */
+class Filter
+{
+    protected $filters = [];
+
+    public function getFilters()
+    {
+        return $this->filters;
+    }
+
+    public function __construct(array $filters = [])
+    {
+        foreach ($filters as $column => $value) {
+            $this->add($column, $value);
+        }
+    }
+
+    public function add($column, $value)
+    {
+        list($column, $operator) = $this->parseColumnName($column);
+
+        $filters = $this->filters[$column] ?? [];
+        $filters[] = [$operator, $value];
+        $this->filters[$column] = $filters;
+    }
+
+    protected function parseColumnName($columnName)
+    {
+        $re = '/^((?:>=|<=)|(?:=|!|@|%|<|>))(.+)$/';
+        if (preg_match($re, $columnName, $m) === 1) {
+            return [$m[2], $m[1]];
+        }
+        else {
+            return [$columnName, '='];
+        }
+    }
+}
