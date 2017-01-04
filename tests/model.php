c13e731e546e93d8a775dbaa18fe848cb10b7e85
irpsv <i.rpsv@live.com>
2017-01-04 13:17:36 +0500

    add validators

--- tests/model.php
+++ tests/model.php
@@ -7,6 +7,9 @@ use jugger\data\field\IntField;
 use jugger\data\field\TextField;
 use jugger\data\field\EnumField;
 use jugger\data\field\BoolField;
+use jugger\data\validator\RangeValidator;
+use jugger\data\validator\RequireValidator;
+use jugger\data\validator\DynamicValidator;
 
 class People extends Model
 {
@@ -15,18 +18,36 @@ class People extends Model
         return [
             new IntField([
                 'name' => 'age',
+                'validators' => [
+                    // возвраст в диапазоне 3-150
+                    new RangeValidator(3, 150)
+                ],
             ]),
             new TextField([
                 'name' => 'fio',
+                'validators' => [
+                    // ФИО с диапазоне 1-15 символов
+                    new RangeValidator(1, 15)
+                ],
             ]),
             new EnumField([
                 'name' => 'sex',
                 'values' => [
                     'man', 'woman'
                 ],
+                'validators' => [
+                    // обязательно с выбранным полом
+                    new RequireValidator()
+                ],
             ]),
             new BoolField([
                 'name' => 'is_superman',
+                'validators' => [
+                    // только супермены
+                    new DynamicValidator(function(bool $value) {
+                        return $value === true;
+                    })
+                ],
             ]),
         ];
     }
@@ -97,5 +118,39 @@ class ModelTest extends TestCase
         $this->assertEquals($people['age'], 456);
         $this->assertFalse(isset($people['key']));
         $this->assertFalse(isset($people[1234]));
+    }
+
+    public function testValidators()
+    {
+        $superman = new People([
+            'age' => 78,
+            'fio' => 'Кларк Кент',
+            'sex' => 'man',
+            'is_superman' => true,
+        ]);
+        $this->assertTrue($superman->validate());
+        $this->assertTrue(count($superman->getErrors()) == 0);
+
+        $superman->age = 666;
+        $this->assertFalse($superman->validate());
+        $this->assertEquals($superman->getError('age'), RangeValidator::class);
+
+        $superman->fio = 'Кларк Джозеф Кент';
+        $this->assertFalse($superman->validate());
+        $this->assertEquals($superman->getError('fio'), RangeValidator::class);
+
+        $superman->sex = null;
+        $this->assertFalse($superman->validate());
+        $this->assertEquals($superman->getError('sex'), RequireValidator::class);
+
+        $superman->is_superman = false;
+        $this->assertFalse($superman->validate());
+        $this->assertEquals($superman->getError('is_superman'), DynamicValidator::class);
+
+        $errors = $superman->getErrors();
+        $this->assertEquals($errors['age'], RangeValidator::class);
+        $this->assertEquals($errors['fio'], RangeValidator::class);
+        $this->assertEquals($errors['sex'], RequireValidator::class);
+        $this->assertEquals($errors['is_superman'], DynamicValidator::class);
     }
 }
