ab47cb23c8d5040c8f9551dde24d8a5c99b41c0e
irpsv <i.rpsv@live.com>
2017-01-04 17:54:48 +0500

    update test

--- tests/dataset.php
+++ tests/dataset.php
@@ -6,86 +6,195 @@ use jugger\data\Filter;
 use jugger\data\Sorter;
 use jugger\data\Paginator;
 use jugger\data\drivers\ArrayDataSet;
+use jugger\data\drivers\QueryDataSet;
+use jugger\data\drivers\ObjectDataSet;
+
+use jugger\db\Query;
+use jugger\db\driver\MysqliConnection;
 
 class DatasetTest extends TestCase
 {
-    public function getData()
+    public static $db;
+
+    public function dataSetProvider()
     {
+        self::setUpBeforeClass();
+
+        $arrayData = $this->getArrayData();
+        $queryData = $this->getQueryData();
+
         return [
-            [1,'name1',123],
-            [2,'name2',456],
-            [3,'name3',789],
-            [4,'name4',123],
-            [5,'name1',456],
-            [6,'name2',789],
-            [7,'name3',123],
-            [8,'name4',456],
-            [9,'name5',789],
+            [
+                $arrayData,
+                new ArrayDataSet($arrayData),
+            ],
+            [
+                $queryData->all(),
+                new QueryDataSet($queryData),
+            ],
         ];
     }
 
-    public function testBase()
+    /**
+     * @dataProvider dataSetProvider
+     */
+    public function testBase($data, $dataset)
     {
-        $data = $this->getData();
-        $dataset = new ArrayDataSet($data);
         $rows = $dataset->getData();
 
         $this->assertEquals(count($data), count($rows));
         for ($i=0; $i<count($data); $i++) {
-            $this->assertEquals($data[$i][0], $rows[$i][0]);
-            $this->assertEquals($data[$i][1], $rows[$i][1]);
-            $this->assertEquals($data[$i][2], $rows[$i][2]);
+            $this->assertEquals($data[$i]['id'], $rows[$i]['id']);
+            $this->assertEquals($data[$i]['name'], $rows[$i]['name']);
+            $this->assertEquals($data[$i]['number'], $rows[$i]['number']);
         }
     }
 
-    public function testSorter()
+    /**
+     * @dataProvider dataSetProvider
+     */
+    public function testSorter($data, $dataset)
     {
-        $data = $this->getData();
-        $dataset = new ArrayDataSet($data);
         $dataset->sorter = new Sorter([
-            1 => Sorter::DESC_NAT,
-            0 => Sorter::ASC,
-            2 => function($a, $b) {
+            'name'   => Sorter::DESC_NAT,
+            'id'     => Sorter::ASC,
+            'number' => function($a, $b) {
                 return 0;
             },
         ]);
         $rows = $dataset->getData();
 
-        $this->assertEquals($rows[0][0], 9);
-        $this->assertEquals($rows[1][0], 4);
-        $this->assertEquals($rows[2][0], 8);
-        $this->assertEquals($rows[3][0], 3);
-        $this->assertEquals($rows[4][0], 7);
-        $this->assertEquals($rows[5][0], 2);
-        $this->assertEquals($rows[6][0], 6);
-        $this->assertEquals($rows[7][0], 1);
-        $this->assertEquals($rows[8][0], 5);
+        $this->assertEquals($rows[0]['id'], 9);
+        $this->assertEquals($rows[1]['id'], 4);
+        $this->assertEquals($rows[2]['id'], 8);
+        $this->assertEquals($rows[3]['id'], 3);
+        $this->assertEquals($rows[4]['id'], 7);
+        $this->assertEquals($rows[5]['id'], 2);
+        $this->assertEquals($rows[6]['id'], 6);
+        $this->assertEquals($rows[7]['id'], 1);
+        $this->assertEquals($rows[8]['id'], 5);
     }
 
-    public function testFilter()
+    /**
+     * @dataProvider dataSetProvider
+     */
+    public function testFilter($data, $dataset)
     {
-        $data = $this->getData();
-        $dataset = new ArrayDataSet($data);
         $dataset->filter = new Filter([
-            '>0' => 5,
-            '@1' => ['name1', 'name2', 'name3'],
-            '=2' => 789
+            '>id' => 5,
+            '@name' => ['name1', 'name2', 'name3'],
+            '=number' => 789
         ]);
         $rows = $dataset->getData();
 
         $this->assertEquals(count($rows), 1);
-        $this->assertEquals($rows[5][0], 6);
+        $this->assertEquals($rows[0]['id'], 6);
     }
 
-    public function testPager()
+    /**
+     * @dataProvider dataSetProvider
+     */
+    public function testPager($data, $dataset)
     {
-        $data = $this->getData();
-        $dataset = new ArrayDataSet($data);
-        $dataset->pager = new Paginator(3, 2);
+        $dataset->sorter = new Sorter([
+            'id' => Sorter::ASC,
+        ]);
+        $dataset->paginator = new Paginator(3, 2);
         $rows = $dataset->getData();
 
-        $this->assertEquals($rows[3][0], 4);
-        $this->assertEquals($rows[4][0], 5);
-        $this->assertEquals($rows[5][0], 6);
+        $this->assertEquals($rows[0]['id'], 4);
+        $this->assertEquals($rows[1]['id'], 5);
+        $this->assertEquals($rows[2]['id'], 6);
     }
+
+
+     public function getArrayData()
+     {
+         return [
+             [
+                 'id' => 1,
+                 'name' => 'name1',
+                 'number' => 123,
+             ],
+             [
+                 'id' => 2,
+                 'name' => 'name2',
+                 'number' => 456,
+             ],
+             [
+                 'id' => 3,
+                 'name' => 'name3',
+                 'number' => 789,
+             ],
+             [
+                 'id' => 4,
+                 'name' => 'name4',
+                 'number' => 123,
+             ],
+             [
+                 'id' => 5,
+                 'name' => 'name1',
+                 'number' => 456,
+             ],
+             [
+                 'id' => 6,
+                 'name' => 'name2',
+                 'number' => 789,
+             ],
+             [
+                 'id' => 7,
+                 'name' => 'name3',
+                 'number' => 123,
+             ],
+             [
+                 'id' => 8,
+                 'name' => 'name4',
+                 'number' => 456,
+             ],
+             [
+                 'id' => 9,
+                 'name' => 'name5',
+                 'number' => 789,
+             ],
+         ];
+     }
+
+     public function getQueryData()
+     {
+         return (new Query(self::$db))
+             ->select(['id', 'name', 'number'])
+             ->from('test_query_dataset');
+     }
+
+     public static function setUpBeforeClass()
+     {
+         self::$db = new MysqliConnection();
+         self::$db->host = "localhost";
+         self::$db->dbname = "test";
+         self::$db->username = "root";
+         self::$db->password = "";
+
+         self::$db->execute("DROP TABLE IF EXISTS `test_query_dataset`");
+         self::$db->execute("
+         CREATE TABLE `test_query_dataset` (
+             `id` INT,
+             `name` TEXT,
+             `number` INT
+         )
+         ");
+         self::$db->execute("INSERT INTO `test_query_dataset` VALUES(1,'name1',123)");
+         self::$db->execute("INSERT INTO `test_query_dataset` VALUES(2,'name2',456)");
+         self::$db->execute("INSERT INTO `test_query_dataset` VALUES(3,'name3',789)");
+         self::$db->execute("INSERT INTO `test_query_dataset` VALUES(5,'name1',456)");
+         self::$db->execute("INSERT INTO `test_query_dataset` VALUES(4,'name4',123)");
+         self::$db->execute("INSERT INTO `test_query_dataset` VALUES(6,'name2',789)");
+         self::$db->execute("INSERT INTO `test_query_dataset` VALUES(7,'name3',123)");
+         self::$db->execute("INSERT INTO `test_query_dataset` VALUES(8,'name4',456)");
+         self::$db->execute("INSERT INTO `test_query_dataset` VALUES(9,'name5',789)");
+     }
+
+     public static function tearDownAfterClass()
+     {
+         self::$db->execute("DROP TABLE IF EXISTS `test_query_dataset`");
+     }
 }
