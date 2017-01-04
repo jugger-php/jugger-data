e4ef9a82437c0f24f9e5ea0b0d118d8233d0dc2e
irpsv <i.rpsv@live.com>
2017-01-04 17:54:44 +0500

    update test

--- tests/pager.php
+++ tests/pager.php
@@ -8,15 +8,22 @@ class PaginatorTest extends TestCase
 {
     public function testInit()
     {
-        $pager = new Paginator(5);
-        $this->assertEquals($pager->pageNow, 1);
-        $this->assertEquals($pager->pageSize, 5);
+        $pager = new Paginator(123, 456);
         $this->assertEquals($pager->totalCount, null);
 
+        $pager = new Paginator(5);
+        $pager->totalCount = 23;
+        $this->assertEquals($pager->pageNow, 1);
+        $this->assertEquals($pager->pageSize, 5);
+        $this->assertEquals($pager->getOffset(), 0);
+        $this->assertEquals($pager->getPageMax(), 5);
+
         $pager = new Paginator(5, 2);
+        $pager->totalCount = 23;
         $this->assertEquals($pager->pageNow, 2);
         $this->assertEquals($pager->pageSize, 5);
-        $this->assertEquals($pager->totalCount, null);
+        $this->assertEquals($pager->getOffset(), 5); // 2-я страница
+        $this->assertEquals($pager->getPageMax(), 5);
 
         $pager = new Paginator(5);
         $pager->pageNow = 123;
@@ -24,7 +31,8 @@ class PaginatorTest extends TestCase
         $pager->totalCount = 789;
         $this->assertEquals($pager->pageNow, 123);
         $this->assertEquals($pager->pageSize, 456);
-        $this->assertEquals($pager->totalCount, 789);
+        $this->assertEquals($pager->getOffset(), 456); // 2-я страница
+        $this->assertEquals($pager->getPageMax(), 2);
     }
 
     /**
